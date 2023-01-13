58428eb7e44b0b42ded2f6919e6a832d8fbcbb6e
giovanna.malzone <giovanna.malzone@accenture.com>
2023-01-13 12:04:27 -0300

    edição seleção de herois

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../heroes.model';
+import { HEROES } from '../mock-heroes';
 
-@Component({
+@Component({ //decorator
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent {
 
+//faz parte do ciclo de vida do component
+export class HeroesComponent implements OnInit{
+  hero: Hero = {
+    id: 1,
+    name: 'Wolverine'
+  };
+
+//pode ser usado para injeção de dependencias
+constructor() {}
+
+ngOnInit(): void {}
+
+heroes = HEROES;
+selectedHero?: Hero;
+
+onSelect(hero: Hero): void{
+  //this = contexto da classe
+  this.selectedHero = hero;
+  }
 }
