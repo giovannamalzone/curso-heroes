ec6972659ee44173a7a7b4caca438556d0dd2768
giovanna.malzone <giovanna.malzone@accenture.com>
2023-01-16 10:38:16 -0300

    edição click

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./heroes.model";
+
+//a lista HEROES é do tipo Hero (interface criada)
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Homem Aranha'},
+  {id: 3, name: 'Batman'},
+  {id: 4, name: 'Superman'},
+  {id: 5, name: 'Mulher Gatp'},
+  {id: 6, name: 'Mulher Maravilha'},
+  {id: 7, name: 'Hulk'},
+  {id: 8, name: 'Capitão América'},
+  {id: 9, name: 'Homem Formiga'},
+  {id: 10, name: 'Capitã'},
+]
